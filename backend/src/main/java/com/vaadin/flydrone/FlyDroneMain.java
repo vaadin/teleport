6f0eee2a60ed9fd2eaaff979b41c6e910b5ed699
Peter Lehto <peter@vaadin.com>
2014-06-23 15:04:34 -0700

    Very basics of the basics to takeoff and land with regular Java App

---
+++ backend/src/main/java/com/vaadin/flydrone/FlyDroneMain.java
@@ -0,0 +1,20 @@
+package com.vaadin.flydrone;
+
+import java.io.IOException;
+
+public class FlyDroneMain {
+
+	private static final String TAKE_OFF = "AT*REF=101,290718208\r";
+	private static final String LAND = "AT*REF=102,290717696\r";
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		try {
+			DroneCommand.executeCommand(TAKE_OFF);
+			Thread.sleep(5000);
+		}
+		finally {
+			DroneCommand.executeCommand(LAND);
+		}
+	}
+}
