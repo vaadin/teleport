3749955c98ed3a4c3cd4f9cb8bd829b26c074cae
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-07-03 20:05:21 -0700

    getting closer

--- backend/src/main/java/com/drone/DroneTemplate2.java
+++ backend/src/main/java/com/drone/DroneTemplate2.java
@@ -1,13 +1,14 @@
 package com.drone;
 
-import static com.drone.ReaderUtils.getUInt32;
+import com.drone.command.*;
+import org.slf4j.Logger;
+import org.springframework.context.Lifecycle;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Arrays;
@@ -17,16 +18,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.slf4j.Logger;
-import org.springframework.context.Lifecycle;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
-
-import com.drone.command.ConfigCommand;
-import com.drone.command.ControlCommand;
-import com.drone.command.DroneCommand;
-import com.drone.command.ResetWatchdogCommand;
+import static com.drone.ReaderUtils.getFloat;
+import static com.drone.ReaderUtils.getUInt32;
 
 public class DroneTemplate2 implements Lifecycle {
 
@@ -34,6 +27,38 @@ public class DroneTemplate2 implements Lifecycle {
 
     public static final int COMMAND_PORT = 5556;
     public static final int NAV_PORT = 5554;
+
+    // supported option tags
+    private static final int CKS_TAG = -1;
+    private static final int DEMO_TAG = 0;
+    private static final int TIME_TAG = 1;
+    private static final int RAW_MEASURES_TAG = 2;
+    private static final int PHYS_MEASURES_TAG = 3;
+    private static final int GYROS_OFFSETS_TAG = 4;
+    private static final int EULER_ANGLES_TAG = 5;
+    private static final int REFERENCES_TAG = 6;
+    private static final int TRIMS_TAG = 7;
+    private static final int RC_REFERENCES_TAG = 8;
+    private static final int PWM_TAG = 9;
+    private static final int ALTITUDE_TAG = 10;
+    private static final int VISION_RAW_TAG = 11;
+    private static final int VISION_OF_TAG = 12;
+    private static final int VISION_TAG = 13;
+    private static final int VISION_PERF_TAG = 14;
+    private static final int TRACKERS_SEND_TAG = 15;
+    private static final int VISION_DETECT_TAG = 16;
+    private static final int WATCHDOG_TAG = 17;
+    private static final int ADC_DATA_FRAME_TAG = 18;
+    private static final int VIDEO_STREAM_TAG = 19;
+    private static final int GAMES_TAG = 20;
+    private static final int PRESSURE_RAW_TAG = 21;
+    private static final int MAGNETO_TAG = 22;
+    private static final int WIND_TAG = 23;
+    private static final int KALMAN_PRESSURE_TAG = 24;
+    private static final int HDVIDEO_STREAM_TAG = 25;
+    private static final int WIFI_TAG = 26;
+    private static final int ZIMMU_3000_TAG = 27;
+
 
     private volatile boolean running = false;
     private final PriorityBlockingQueue<DroneCommand> commands = new PriorityBlockingQueue<>(
@@ -77,8 +102,7 @@ public class DroneTemplate2 implements Lifecycle {
     private DroneState readDroneState(DatagramSocket datagramSocket,
                                       int maxPacketSize) throws IOException {
 
-        DatagramPacket packet = new DatagramPacket(new byte[maxPacketSize],
-                maxPacketSize);
+        DatagramPacket packet = new DatagramPacket(new byte[maxPacketSize], maxPacketSize);
         datagramSocket.receive(packet);
 
         ByteBuffer buffer = ByteBuffer.wrap(packet.getData(), 0,
@@ -92,25 +116,70 @@ public class DroneTemplate2 implements Lifecycle {
         DroneState droneState = new DroneState(sequence, state, vision);
         logger.info("droneState: " + droneState);
 
-        if (droneState.isNavDataDemoOnly()) {
-            while (buffer.position() < buffer.limit()) {
-                int tag = buffer.getShort() & 0xFFFF;
-                int payloadSize = (buffer.getShort() & 0xFFFF) - 4;
-                ByteBuffer optionData = buffer.slice().order(
-                        ByteOrder.LITTLE_ENDIAN);
-                payloadSize = Math.min(payloadSize, optionData.remaining());
-                optionData.limit(payloadSize);
-                logger.info("Parsin tag " + tag);
-                parseOption(tag, optionData);
-                buffer.position(buffer.position() + payloadSize);
-            }
+        // parse options
+        ByteBuffer b = buffer;
+        int cks = 0;
+        while (b.position() < b.limit()) {
+            int tag = b.getShort() & 0xFFFF;
+            int payloadSize = (b.getShort() & 0xFFFF) - 4;
+            logger.info("tag: " + tag + " payloadSize:  " + payloadSize);
+            ByteBuffer optionData = b.slice().order(ByteOrder.LITTLE_ENDIAN);
+            payloadSize = Math.min(payloadSize, optionData.remaining());
+            optionData.limit(payloadSize);
+            int cksOption = parseOption(tag, optionData);
+            if (cksOption != 0)
+                cks = cksOption;
+            b.position(b.position() + payloadSize);
         }
+
+
+        if (0 != cks)
+            Assert.isTrue(ReaderUtils.getCRC(b, 0, b.limit() - 4) == cks, "Checksum does not match");
 
         return droneState;
     }
 
-    private void parseOption(int tag, ByteBuffer optionData) {
-        if (tag == 0) {
+
+    private void parseAltitudeOption(ByteBuffer b) {
+        // if (altitudeListener.size() > 0) {
+        int altitude_vision = b.getInt();
+        float altitude_vz = b.getFloat();
+        int altitude_ref = b.getInt();
+        int altitude_raw = b.getInt();
+
+        // TODO: what does obs mean?
+        float obs_accZ = b.getFloat();
+        float obs_alt = b.getFloat();
+
+        float[] obs_x = getFloat(b, 3);
+
+        int obs_state = b.getInt();
+
+        // TODO: what does vb mean?
+        float[] est_vb = getFloat(b, 2);
+
+        int est_state = b.getInt();
+
+
+        logger.info("altitude : vision " + altitude_vision + " vz " + altitude_vz + "ref " +
+                altitude_ref + " raw " + altitude_raw);
+
+
+    }
+
+    private int parseOption(int tag, ByteBuffer optionData) {
+        if (tag == WIFI_TAG) {
+            //  getUInt16( optionData) ; // tag
+            //getUInt16( optionData) ; // size
+            long linkQuality = getUInt32(optionData); //
+            logger.info("linkQuality: " + linkQuality);
+        }
+
+        if (tag == ALTITUDE_TAG) {
+            this.parseAltitudeOption(optionData);
+        }
+
+        if (tag == DEMO_TAG) {
             int parsedTag = optionData.getShort();
             int parsedSize = optionData.getShort();
             int battery = optionData.getInt();
@@ -119,11 +188,19 @@ public class DroneTemplate2 implements Lifecycle {
             float psi = optionData.getFloat();
             int altitude = optionData.getInt();
 
-            System.out.println("tag:  " + parsedTag + " size: " + parsedSize
-                    + " battery: " + battery + " theta: " + theta + " phi: "
-                    + phi + " psi: " + psi + " altitude: " + altitude);
+            logger.info(//"tag:  " + parsedTag + " size: " + parsedSize+
+                    "battery: " + battery + " theta: " + theta + " phi: "
+                            + phi + " psi: " + psi + " altitude: " + altitude);
 
         }
+
+        int checksum = 0;
+        if (tag == CKS_TAG) {
+            checksum = optionData.getInt();
+            logger.info("checksum=" + checksum);
+        }
+
+        return checksum;
     }
 
     private void enqueueCommand(DroneCommand command) {
@@ -144,17 +221,55 @@ public class DroneTemplate2 implements Lifecycle {
         navDataSocket.setSoTimeout(3000);
 
         // tickle!
-        navDataSocket.send(new DatagramPacket(new byte[] { 0x01, 0x00, 0x00,
-                0x00 }, 4, address, navPort));
+        navDataSocket.send(new DatagramPacket(new byte[]{0x01, 0x00, 0x00,
+                0x00}, 4, address, navPort));
 
         // get the deon
         DroneState droneState = readDroneState(navDataSocket, maxPacketSize);
         bootstrapNavData(droneState);
 
+        enqueueCommand(new ConfigCommand("general:navdata_demo", false));
+        enqueueCommand(new ConfigCommand("general:navdata_options", maskForOptions(ALTITUDE_TAG, CKS_TAG, DEMO_TAG, WIFI_TAG)));
+
+
         while (isRunning()) {
             droneState = readDroneState(navDataSocket, maxPacketSize);
 
+            if (droneState.isCommunicationProblemOccurred()) {
+                // todo resetCommunicationWatchDog
+            }
+
+
+
+
+            /*	// detect control Ack change
+                boolean newcontrolAck = s.isControlReceived();
+				if (newcontrolAck != controlAck) {
+					manager.setControlAck(newcontrolAck);
+					controlAck = newcontrolAck;
+				}
+
+				// TODO should we reset the communication watchdog always?
+				if (s.isCommunicationProblemOccurred()) {
+					manager.resetCommunicationWatchDog();
+				}
+
+				// TODO bootstrapping probably be handled by commandmanager
+				if (!bootstrapping && maskChanged) {
+					manager.setNavDataDemo(false);
+					manager.setNavDataOptions(mask);
+					maskChanged = false;
+				}*/
         }
+    }
+
+    private int maskForOptions(int... tags) {
+        int mask = 0, newmask = 0;
+        for (int tag : tags) {
+            newmask |= 1 << tag;
+        }
+        mask |= newmask;
+        return mask;
     }
 
     private void commandLoop(InetAddress address, DatagramSocket socket,
@@ -180,7 +295,7 @@ public class DroneTemplate2 implements Lifecycle {
                     // sticky commands replace previous sticky
                     stickyCommand = command;
                     lastRecordedTime = System.currentTimeMillis();
-                } else /* if (command.clearSticky()) */{
+                } else /* if (command.clearSticky()) */ {
                     // only some commands can clear sticky commands
                     stickyCommand = null;
                 }
@@ -262,8 +377,16 @@ public class DroneTemplate2 implements Lifecycle {
                 throw new RuntimeException(e);
             }
         });
+        try {
+            enqueueCommand(new TakeOffCommand());
+            Thread.sleep(1000 * 10);
+        } finally {
+            enqueueCommand(new LandCommand());
+        }
+
 
     }
+
 
     private static InetAddress fromIP(String ip) {
         ip = StringUtils.hasText(ip) ? ip : DEFAULT_IP;
